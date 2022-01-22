afaf3f367beef0fbc1d3a1394f72c280fd1a7a1a
Brianyyds <haha3316@gmail.com>
2022-01-22 11:02:34 +0800

    0122 merge googlemap

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,16 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = (app) => {
-  app.use(
-    createProxyMiddleware("/maps/api/place/textsearch/json", {
-      target: "https://maps.googleapis.com",
-      changeOrigin: true,
-    })
-  );
-};
+const { createProxyMiddleware } =require("http-proxy-middleware") ;
+module.exports = app =>{
+    app.use(
+        createProxyMiddleware("/maps/api/place/details/json",{
+        target:"https://maps.googleapis.com",
+        changeOrigin:true
+
+    }))
+    app.use(
+        createProxyMiddleware("/maps/api/distancematrix/json",{
+
+        target:"https://maps.googleapis.com",
+        changeOrigin:true
+
+    }))
+}
