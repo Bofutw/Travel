bd6d4ff0c0259bd0a019f733f882681dcf147c1b
bofu <bofusoci@gmail.com>
2022-01-24 14:56:45 +0800

    update collect

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Zoom from '@mui/material/Zoom';
+
 import './favorite.css'
 
+let memberid =window.localStorage.memberid
 const Favorite = () => {
+
+    const [data, setData] = useState([]);
+
+    function fetchData(){
+        fetch("http://localhost:8080/blog/memberid="+memberid)
+        .then((res)=>{
+          return res.json();
+        })
+        .then((result)=>{
+          console.log(result)
+          setData(result)
+      }
+    
+      )
+    }
+    useEffect(fetchData,[]);
     return (
         <div >
         {/* <div>
@@ -21,40 +46,52 @@ const Favorite = () => {
                 <div class="page__content" id='bloglist'>
                 {/* <h1 class="page__content-title">I</h1> */}
                 <ul class='favorite' >
-                    <li><a href='https://github.com/EEIT36-Travel'>星期一二三</a></li>
+                
+
+                {
+                    data.map((item,index)=>{
+                        let test = JSON.parse(item.blogdetail).title;
+                        return(
+                            <>
+                            {/* <li>
+                                <a href='#'><h3>{test}</h3></a>
+                                
+                                {JSON.parse(item.blogdetail).decrption}
+                                <br></br> <br></br>
+                            </li> */}
+                             <Zoom in='true' timeout={(index+6)*1000}>
+                             <Card sx={{ maxWidth: 345 , marginBottom: 3}}>
+                             <CardMedia
+                               component="img"
+                               alt=""
+                               height="140"
+                               image= {JSON.parse(item.blogdetail).url}
+                             />
+                             <CardContent>
+                               <Typography gutterBottom variant="h5" component="div">
+                               {test}
+                               </Typography>
+                               <Typography variant="body2" color="text.secondary">
+                               {JSON.parse(item.blogdetail).decrption}
+                               </Typography>
+                             </CardContent>
+                             <CardActions>
+                               <Button size="small">Share</Button>
+                               <Button size="small">Learn More</Button>
+                             </CardActions>
+                           </Card>
+                           </Zoom>
+                           </>   
+                        )
+                    })
+                }
+                  {/*   <li><a href='https://github.com/EEIT36-Travel'>星期一二三</a></li>
                     <li>星期二三四五六</li>
                     <li>星期一四五</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
-                    <li>星期一</li>
-                    <li>星期二</li>
+                    <li>星期二</li> */}
+              
+                 
+                    
                     
                 </ul>
                 {/* <div class="page__content-blockquote">
