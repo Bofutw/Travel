ed08a57a0ca2572a8a4a79fbce393801f0efbb96
Brianyyds <haha3316@gmail.com>
2022-01-28 14:42:55 +0800

    .

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
-// module.exports = (app) => {
-//   app.use(
-//     createProxyMiddleware("/maps/api/place/textsearch/json", {
-//       target: "https://maps.googleapis.com",
-//       changeOrigin: true,
-//     })
-//   );
-// };
