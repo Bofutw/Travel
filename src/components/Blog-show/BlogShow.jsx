108acdfe71d52f21e816bb698c502a293bdb8d63
Raven Chen <rex27831@gmail.com>
2022-01-28 10:38:34 +0800

    rightside

--- src/components/Blog-show/BlogShow.jsx
+++ src/components/Blog-show/BlogShow.jsx
@@ -1,180 +1,184 @@
-import React ,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './blogshow.css'
 
 
 
 export default function BlogShow() {
-    let blogdata ;
-    
-     blogdata = JSON.parse(window.localStorage.blogdata);
-     blogdata.blogdetail = JSON.parse(blogdata.blogdetail);
-     const [populardata,setPopulardata] = useState([]);
-     const [collectstatus,setCollectstatus] = useState([]);
-     const [collectid,setCollectid] = useState();
-    useEffect(()=>{
+    let blogdata;
+
+    blogdata = JSON.parse(window.localStorage.blogdata);
+    blogdata.blogdetail = JSON.parse(blogdata.blogdetail);
+    const [populardata, setPopulardata] = useState([]);
+    const [collectstatus, setCollectstatus] = useState([]);
+    const [collectid, setCollectid] = useState();
+    useEffect(() => {
         getPopularData();
         collectStatusCheck();
-    },[]);
-     function getPopularData(){
+    }, []);
+    function getPopularData() {
         fetch("http://localhost:8080/blog/topblog")
-        .then((res)=>{
-            
-            return res.json()
-        })
-        .then((result)=>{
-            
-            for(let i =0;i<result.length;i++){
-                console.log(result[i].blogdetail)
-                result[i].blogdetail = JSON.parse(result[i].blogdetail)
-            }
-            console.log(result)
-            setPopulardata(result)
-        })
+            .then((res) => {
+
+                return res.json()
+            })
+            .then((result) => {
+
+                for (let i = 0; i < result.length; i++) {
+                    console.log(result[i].blogdetail)
+                    result[i].blogdetail = JSON.parse(result[i].blogdetail)
+                }
+                console.log(result)
+                setPopulardata(result)
+            })
     }
-    function toBlogPage(e){
-       
-        fetch("http://localhost:8080/blog/"+(e.target.id).slice(6,8))
-        .then((res)=>{           
-            return res.json()
-        })
-        .then((result)=>{
-            window.localStorage.blogdata = JSON.stringify(result)
-            window.location.href = "/Blogshow"
- 
-        })
+    function toBlogPage(e) {
+
+        fetch("http://localhost:8080/blog/" + (e.target.id).slice(6, 8))
+            .then((res) => {
+                return res.json()
+            })
+            .then((result) => {
+                window.localStorage.blogdata = JSON.stringify(result)
+                window.location.href = "/Blogshow"
+
+            })
     }
-    function collectStatusCheck(){
+    function collectStatusCheck() {
         fetch(`http://localhost:8080/collect/status/memberid=${window.localStorage.memberid}&blogid=${JSON.parse(window.localStorage.blogdata).blogid}`)
-        .then((res)=>{
-            return res.json()
-        })
-        .then((result)=>{
-            
-            window.localStorage.collectid = result[1];
-            setCollectstatus(result[0]) ;
-        })
+            .then((res) => {
+                return res.json()
+            })
+            .then((result) => {
+
+                window.localStorage.collectid = result[1];
+                setCollectstatus(result[0]);
+            })
 
     }
-    function insertCollect(){
+    function insertCollect() {
         fetch(`http://localhost:8080/collect/memberid=${window.localStorage.memberid}&blogid=${JSON.parse(window.localStorage.blogdata).blogid}`,
-        {
-            method:"POST"
+            {
+                method: "POST"
 
-        })
-        .then((res)=>{
-            
-            return res.json();
-            
-        }).then((result)=>{
-           
-            window.localStorage.collectid = result.collectid
-            window.location.reload();
-        })
-        
-    } 
-    function deleteCollect(){
-        fetch(`http://localhost:8080/collect/${window.localStorage.collectid}`,{
-            method:"DELETE"
-        })
-        .then((res)=>{
-            window.location.reload();
+            })
+            .then((res) => {
 
+                return res.json();
+
+            }).then((result) => {
+
+                window.localStorage.collectid = result.collectid
+                window.location.reload();
+            })
+
+    }
+    function deleteCollect() {
+        fetch(`http://localhost:8080/collect/${window.localStorage.collectid}`, {
+            method: "DELETE"
         })
+            .then((res) => {
+                window.location.reload();
+
+            })
 
     }
     return (
-        
-        <div style={{display: 'grid',gridTemplateColumns:'70% 30%'}}>
+
+        <div style={{ display: 'grid', gridTemplateColumns: '70% 30%' }}>
             <div>
-                <div class="bls" style={{ width:'1000px',height:'350px',margin: '70px',display: 'flex',justifyContent: 'center',alignItems: 'center'}}>
-                    <img src={blogdata.blogdetail.url} style={{width: '100%',height: '100%',objectFit:'cover'}}></img>
+                <div class="bls" style={{ width: '1000px', height: '350px', margin: '70px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <img src={blogdata.blogdetail.url} style={{ width: '100%', height: '100%', objectFit: 'cover' }}></img>
                 </div>
-                <div style={{marginLeft:'100px', marginTop:"-50px",marginBottom:'60px',width:'900px'}}>
+                <div style={{ marginLeft: '100px', marginTop: "-50px", marginBottom: '60px', width: '900px' }}>
                     <h1>{blogdata.blogdetail.title}</h1>
                     <h5>{blogdata.blogdetail.decrption}</h5>
                 </div>
-                <button style={{marginLeft:'100px', marginTop:"-50px",marginBottom:'60px'}} onClick={collectstatus?deleteCollect:insertCollect}>{collectstatus?"取消收藏":"收藏"}</button>
+                <button style={{ marginLeft: '100px', marginTop: "-50px", marginBottom: '60px' }} onClick={collectstatus ? deleteCollect : insertCollect}>{collectstatus ? "取消收藏" : "收藏"}</button>
                 <div >
-                    <div class="leftcolumn"style={{backgroundColor:'',overflow: 'auto',height:'1000px'}}>
-                        
-                    {blogdata.blogdetail.eachDay.map((item,idex)=>{
-                        let list=[];
-                        for(let i = 0;i<item.eachplace.length;i++){
-                            list.push(<div class="card"style={{display: 'grid',gridTemplateColumns:'65% 35%'}}>
-                            <div >
-                                <h3><i class="fa fa-map-marker" aria-hidden="true"></i>  {item.eachplace[i].subTitle}</h3>
-                                <p id='blogshowcard-p' > {item.eachplace[i].text}</p>
-                                
-                            </div>
-                            <div>
-                            <img src={item.eachplace[i].pic} style={{width:'350px',height:'300px'}}></img>
-                           
-                            
-                            </div>
-                        </div>)
-                        }
-                        return <><h2 id={idex} style={{boxShadow:'0 2px',paddingBottom:'10px',width:'400px',marginTop:'60px'}}><i class="fa fa-map-o" aria-hidden="true"></i>  第{idex+1}天</h2>{list}</>
-                        
+                    <div class="leftcolumn" style={{ backgroundColor: '', overflow: 'auto', height: '1000px' }}>
+
+                        {blogdata.blogdetail.eachDay.map((item, idex) => {
+                            let list = [];
+                            for (let i = 0; i < item.eachplace.length; i++) {
+                                list.push(<div class="card" style={{ display: 'grid', gridTemplateColumns: '65% 35%' }}>
+                                    <div >
+                                        <h3><i class="fa fa-map-marker" aria-hidden="true"></i>  {item.eachplace[i].subTitle}</h3>
+                                        <p id='blogshowcard-p' > {item.eachplace[i].text}</p>
+
+                                    </div>
+                                    <div>
+                                        <img src={item.eachplace[i].pic} style={{ width: '330px', height: '300px' }}></img>
+
+
+                                    </div>
+                                </div>)
+                            }
+                            return <><h2 id={idex} style={{ boxShadow: '0 2px', paddingBottom: '10px', width: '400px', marginTop: '60px' }}><i class="fa fa-map-o" aria-hidden="true"></i>  第{idex + 1}天</h2>{list}</>
 
 
 
-                    })}
+
+                        })}
                     </div>
-                    
+
                 </div>
             </div>
             <div>
-            <div class="rightcolumn">
-                <div class="card1">
-                
-                <div class="" ><img alt="Avatar" src={blogdata.membericon}style={{height:"100px",width:'100px',"border-radius": "50%"}}/>{` 作者:${blogdata.membernickname}`}</div>
-                <br/>
-                <p>{blogdata.memberintro}</p>
-                </div>
-                <div class="card1">
-                <h3>旅遊目錄</h3>
-                <nav>
-                    <ul>
-                        {blogdata.blogdetail.eachDay.map((item,idex)=>{
-                            return <li><a href={`#${idex}`}>第{idex+1}天</a></li>
-                        })}
- 
-                    </ul>
-                </nav>
-                </div>
-                <div class="card1">
-                <div style={{ width: '300px', height: '650px', backgroundColor: 'rgba(161,228,241,0.6)', borderRadius: '5px', marginTop: '15px' }}>
-                                <div style={{ paddingTop: "15px", color: 'black', paddingLeft: '35px', marginBottom: '30px' }}>
-                                    <h4><i class="fa fa-star" aria-hidden="true" style={{ marginRight: '15px' }}></i>熱門文章</h4>
-                                </div>
-                                <ul class="details">
-                                    {populardata.map((item,id)=>{
-                                        if(id<3){
-                                            return <li onClick={toBlogPage}class="" style={{ height: '110px', marginBottom: '70px',cursor: 'pointer' }} >
+                {/* 此處有更改 */}
+                <div class="rightcolumn">
+                    <div style={{ width: '300px', height: '450px', backgroundColor: 'rgba(0,0,0,0.6)', borderRadius: '5px', marginTop: '15px', marginLeft: '20px' }}>
+                        <div class="card1" style={{backgroundColor:'transparent'}}>
+
+                            <div class=""  style={{color:'white',fontSize:'24px'}}><img alt="Avatar" src={blogdata.membericon} style={{ height: "100px", width: '100px', "border-radius": "50%" ,marginRight:'10px'}} />{` ${blogdata.membernickname}`}</div>
+                            <br />
+                            <p style={{color:'white'}}>{blogdata.memberintro}</p>
+                        </div>
+                        <div class="card1" style={{backgroundColor:'transparent'}}>
+                            <h4 style={{color:'white',fontWeight:'bold'}}><i class="fa fa-newspaper-o" aria-hidden="true"style={{ marginRight: '10px' }}></i>旅遊目錄</h4>
+                            <nav style={{color:'white'}}>
+                                <ul>
+                                    {blogdata.blogdetail.eachDay.map((item, idex) => {
+                                        return <li><a href={`#${idex}`} id='blogshow-day' style={{marginLeft:'37px',resize:'none',overflow:"scroll"}}>第{idex + 1}天</a></li>
+                                    })}
+
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="card1">
+                        {/* 此處有更改 */}
+                        <div style={{ width: '300px', height: '650px', backgroundColor: 'rgba(0,0,0,0.6)', borderRadius: '5px' }}>
+                            <div style={{ paddingTop: "15px", color: 'white', paddingLeft: '25px', marginBottom: '30px' }}>
+                                <h4 style={{fontWeight:'bold'}}><i class="fa fa-star" aria-hidden="true" style={{ marginRight: '15px' }}></i>熱門文章</h4>
+                            </div>
+                            <ul class="details">
+                                {populardata.map((item, id) => {
+                                    if (id < 3) {
+                                        return <li onClick={toBlogPage} class="" style={{ height: '110px', marginBottom: '70px', cursor: 'pointer' }} >
                                             <div class="blog-card" style={{ height: '160px', width: '260px' }}>
                                                 <div class="meta">
                                                     <div id={`blogid${item.blogid}`} class="photo" style={{ backgroundImage: `url(${item.blogdetail.url})`, height: '160px', width: '260px' }}></div>
                                                     {/* `url(${item.blogdetail.url})` */}
                                                     <ul id={`blogid${item.blogid}`} class="details" style={{ height: '160px', width: '105px' }}>
                                                         <li class="fa fa-pencil" id={`blogid${item.blogid}`}>     {item.blogdetail.title}</li>
-                                                        <li class="date" id={`blogid${item.blogid}`}>{item.blogcreatetime.slice(0,10)}</li>
-    
+                                                        <li class="date" id={`blogid${item.blogid}`}>{item.blogcreatetime.slice(0, 10)}</li>
+
                                                     </ul>
                                                 </div>
                                             </div>
                                         </li>
-                                        }
+                                    }
 
-                                    })}
+                                })}
 
-                                </ul>
+                            </ul>
 
-                            </div>
-                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
