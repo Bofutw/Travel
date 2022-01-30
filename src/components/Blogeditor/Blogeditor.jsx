b5080058d0c9db1d9f956ed4e8833b7a0ecece1a
bofu <bofusoci@gmail.com>
2022-01-30 14:08:35 +0800

    add drag upload

--- src/components/Blogeditor/Blogeditor.jsx
+++ src/components/Blogeditor/Blogeditor.jsx
@@ -1,14 +1,18 @@
 import { WindowOutlined } from '@mui/icons-material'
-import React from 'react'
+import React, { useState } from 'react'
 import './blogeditor.css'
 import { useEffect } from 'react'
 
 import TextField from '@mui/material/TextField';
+import { DropzoneArea } from 'material-ui-dropzone';
+import { Button } from '@mui/material';
+
 
 
 // "{\"beginDate\":\"2022-01-19\",\"daysNum\":4,\"eachDays\":[{\"eachPlaces\":[{\"placeName\":\"南屯國小\",\"AttractionsId\":\"ChIJ0ZMoF8k9aTQRuCDDop7t6kc\"},{\"placeName\":\"北屯國小\",\"AttractionsId\":\"ChIJHQYfVeAXaTQRnAVJXNo3vIE\"}]},{\"eachPlaces\":[{\"placeName\":\"北區醫院\",\"AttractionsId\":\"ChIJC5WkxxX2AzQRfGB1A1MRrh8\"}]},{\"eachPlaces\":[{\"placeName\":\"西屯國小\",\"AttractionsId\":\"ChIJUyVn5BgWaTQR9DFV9fYc0BY\"}]}]}"
 // {"blogid":13,"blogdetail":"{\r\n    \"title\":\"集中營3日遊\",\r\n    \"decrption\":\"decrption\",\r\n    \"eachDay\":[\r\n        {\r\n            \"eachplace\":[\r\n                {\r\n                    \"subTitle\":\"集中營\",\r\n                    \"text\":\"真猶你的\",\r\n                    \"pic\":[\"\",\"\"],\r\n                    \"AttractionsId\":\"123\"\r\n                },\r\n                {\r\n                    \"subTitle\":\"毒氣室\",\r\n                    \"text\":\"..........\",\r\n                    \"pic\":[\"\",\"\"],\r\n                    \"AttractionsId\":\"7777777\"\r\n                },\r\n                {\r\n                    \"subTitle\":\"這裡超好玩\",\r\n                    \"text\":\"內文blablablablabal..........\",\r\n                    \"pic\":[\"\",\"\"],\r\n                    \"AttractionsId\":\"3\"\r\n                }\r\n            \r\n            ]                \r\n        },\r\n        {\r\n            \"eachplace\":[\r\n                {\r\n                    \"subTitle\":\"古色古香的咖啡廳\",\r\n                    \"text\":\"1111111..........\",\r\n                    \"pic\":[\"\",\"\"],\r\n                    \"AttractionsId\":\"123\"\r\n                },\r\n                {\r\n                    \"subTitle\":\"24小時營業的而且還賣滷味的家具店\",\r\n                    \"text\":\"內文blablablablabal..........\",\r\n                    \"pic\":[\"\",\"\"],\r\n                    \"AttractionsId\":\"7777777\"\r\n                },\r\n                {\r\n                    \"subTitle\":\"這裡超好玩\",\r\n                    \"text\":\"內文blablablablabal..........\",\r\n                    \"pic\":[\"\",\"\"],\r\n                    \"AttractionsId\":\"3\"\r\n                }\r\n            \r\n            ]                \r\n        },\r\n            {\r\n                \"eachplace\":[\r\n                    {\r\n                        \"subTitle\":\"古色古香的咖啡廳\",\r\n                        \"text\":\"1111111..........\",\r\n                        \"pic\":[\"\",\"\"],\r\n                        \"AttractionsId\":\"123\"\r\n                    },\r\n                    {\r\n                        \"subTitle\":\"24小時營業的而且還賣滷味的家具店\",\r\n                        \"text\":\"內文blablablablabal..........\",\r\n                        \"pic\":[\"\",\"\"],\r\n                        \"AttractionsId\":\"7777777\"\r\n                    },\r\n                    {\r\n                        \"subTitle\":\"這裡超好玩\",\r\n                        \"text\":\"內文blablablablabal..........\",\r\n                        \"pic\":[\"\",\"\"],\r\n                        \"AttractionsId\":\"3\"\r\n                    }               \r\n                ]                \r\n            }\r\n    ]\r\n}","blogauthority":0,"blogcreatetime":"2022-01-16T16:48:34.000+00:00","blogupdatetime":null,"blogpopular":7}
 export default function BlogShow() {
+    const [upload, setOpload] = useState(false);
     let blog;
     let data;
     function initblog() {
@@ -116,6 +120,9 @@ export default function BlogShow() {
             })
     }
     function imgchange(e) {
+        console.log(e.target.files[0]);
+        console.log(e.target.id);
+        console.log(e.target.files);
         let url = URL.createObjectURL(e.target.files[0])
         document.getElementById((e.target.id).substr(0, e.target.id.length - 5)).setAttribute("src", url)
 
@@ -131,6 +138,24 @@ export default function BlogShow() {
         }
         imagesave(e.target.files[0], logicname)
     }
+    //
+    function imgchange2(files) {
+       
+       if(files[0]!=undefined){
+           
+            setOpload(true);
+          
+            let url = URL.createObjectURL(files[0]) 
+            document.getElementById('topimg2').hidden=false
+           document.getElementById('topimg2').setAttribute("src", url)  
+            let date = new Date();
+           let logicname = files[0].name + date.toISOString();
+           blog.blogdetail.url = `https://storage.googleapis.com/travelproject/${logicname}.png`
+           imagesave(files[0], logicname) 
+          
+       }
+    }
+    //
     function getMemberNickName() {
         fetch("http://localhost:8080/member/" + window.localStorage.memberid)
             .then((res) => {
@@ -141,16 +166,33 @@ export default function BlogShow() {
             })
     }
     initblog();
+
+    
     return (
 
         <div style={{ display: 'grid', gridTemplateColumns: '80% 20%' }}>
 
             <div>
-                <div class="bls" style={{ width: '1000px', height: '350px', margin: '70px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                {/* <div class="bls" style={{ width: '1000px', height: '350px', margin: '70px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                     <input id='topimginput' type={"file"} onChange={imgchange}></input>
                     <img id='topimg' src={blog.blogdetail.url} style={{ width: '100%', height: '100%', objectFit: 'cover' }}></img>
-                </div>
-                <div style={{ marginLeft: '100px', marginTop: "-50px", marginBottom: '60px', width: '900px' }}>
+                </div> */}
+                <div style={{ marginLeft: '100px', marginTop: "50px", marginBottom: '60px', width: '900px' }}>
+                {upload==true?
+                <div></div>       
+                :<DropzoneArea
+                acceptedFiles={['image/*']}
+                dropzoneText={"請將封面圖片拖曳至此 或透過點擊增加"}
+                onChange={imgchange2}
+                filesLimit='1'
+                fullWidth
+                />
+                }
+                <img id='topimg2' hidden='true' src={blog.blogdetail.url} style={{ width: '100%', height: '100%', objectFit: 'cover' }}></img>
+                
+              
+                    <br></br><br></br>
+
                     <TextField
                         id="titlearea"
                         label="標題"
@@ -175,12 +217,12 @@ export default function BlogShow() {
                         multiline='true'
                         minRows='4'
                         onChange={changeText}
-                       
+
                     />
-                  {/*   <textarea onChange={changeText} id="titlearea">{(blog.blogdetail.title) ? blog.blogdetail.title : "為您的旅程設下一個標題...."}</textarea>
+                    {/*   <textarea onChange={changeText} id="titlearea">{(blog.blogdetail.title) ? blog.blogdetail.title : "為您的旅程設下一個標題...."}</textarea>
                     <textarea onChange={changeText} id="decrptionarea">{(blog.blogdetail.decrption) ? blog.blogdetail.decrption : "為您的旅程增加一些描述...."}</textarea>
-             */}    
-             </div>
+             */}
+                </div>
                 <div >
                     <div class="leftcolumn" style={{ backgroundColor: '', overflow: 'auto', height: '1000px' }}>
                         <button onClick={save}>save</button>
@@ -195,7 +237,8 @@ export default function BlogShow() {
 
                                     </div>
                                     <div>
-                                        <input id={`img${idex}${i}input`} type={"file"} onChange={imgchange}></input>
+                                       
+                                        <input id={`img${idex}${i}input`} type={"file"} onChange={imgchange} name={'test'}></input>
                                         <img id={`img${idex}${i}`} src={item.eachplace[i].pic} style={{ width: '350px', height: '295px', objectFit: 'cover' }}></img>
 
 
